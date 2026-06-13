LineageOS/android_bootable_recovery

recovery: Bringup network all the times
* For ADB mode (on devices without USB Gadget mode, same for fastboot)

Change-Id: Ia5937e71e4800872970b568152c5e5d6d722bf04

recovery_main.cpp
@@ -523,6 +523,8 @@ int main(int argc, char** argv) {
     android::base::SetProperty("service.adb.root", "1");
   }
 
+  device->InitBringupNetwork();
+
   while (true) {
     // We start adbd in recovery for the device with userdebug build or a unlocked bootloader.
     std::string usb_config =

recovery_ui/include/recovery_ui/device.h
@@ -90,6 +90,9 @@ class Device {
     ui_.reset(ui);
   }
 
+  // Called before any mode started up, to bring up network.
+  virtual void InitBringupNetwork() {}
+
   // Called before recovery mode started up, to perform whatever device-specific recovery mode
   // preparation as needed.
   virtual void PreRecovery() {}

alternoegraha/lineage_bootable_recovery

recovery_main.cpp
@@ -514,6 +514,8 @@ int main(int argc, char** argv) {
     android::base::SetProperty("service.adb.root", "1");
   }
 
+  device->InitDevice();
+
   while (true) {
     // We start adbd in recovery for the device with userdebug build or a unlocked bootloader.
     std::string usb_config =

recovery_ui/include/recovery_ui/device.h
@@ -90,6 +90,9 @@ class Device {
     ui_.reset(ui);
   }
 
+  // Called before any mode started up, to bring up network.
+  virtual void InitDevice() {}
+
   // Called before recovery mode started up, to perform whatever device-specific recovery mode
   // preparation as needed.
   virtual void PreRecovery() {}